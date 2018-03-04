5594ad92ab80fdde4e1b1f465fd381338d0459c8
Yanb <yann-elias@wanadoo.fr>
2018-03-04 12:54:19 +0100

    Complete README

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Csa\Bundle\GuzzleBundle\CsaGuzzleBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
